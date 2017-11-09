1c6f02de0a2225c980bfdc10eb3c22f9081f912b
mustafa.suve <suvemustafa@gmail.com>
2017-11-09 11:55:27 +0300

    Database save, insert, select done postman must be downloaded from chrome store

--- src/main/java/com/mustafa/test/Test/INewTableRepo.java
+++ src/main/java/com/mustafa/test/Test/INewTableRepo.java
@@ -1,9 +1,11 @@
 package com.mustafa.test.Test;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface INewTableRepo extends JpaRepository<NewTable, Long> {
 
-	
+	public List<NewTable> findByName(String name);
 	
 }
--- src/main/java/com/mustafa/test/Test/NewTableController.java
+++ src/main/java/com/mustafa/test/Test/NewTableController.java
@@ -3,7 +3,9 @@ package com.mustafa.test.Test;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +17,30 @@ public class NewTableController {
 	@RequestMapping(path = "/")
 	public List<NewTable> getall() {
 		return repo.findAll();
+	}
+	
+	@RequestMapping(path = "/getid/{id}")
+	public NewTable getById(@PathVariable Long id) {
+		return repo.findOne(id);
+	}
+	
+	@RequestMapping(path = "/getname/{name}")
+	public List<NewTable> getByName(@PathVariable String name) {
+		return repo.findByName(name);
+	}
+	
+	@RequestMapping(path = "/save/{id}/{name}", method = RequestMethod.POST)
+	public String save(@PathVariable Long id, @PathVariable String name) {
+		try {
+			NewTable entry = new NewTable();
+			entry.setId(id);
+			entry.setName(name);
+			repo.save(entry);
+			return "Saved Successful";
+		} catch(Throwable e) {
+			System.out.println("Save Failed" + e.getMessage());
+			return "Save Failed";
+		}
 	}
 	
 }
