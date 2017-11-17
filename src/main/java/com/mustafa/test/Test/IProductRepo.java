ef820eabdc00a2f9ac6958f6c0c213e1cafae266
mustafa.suve <suvemustafa@gmail.com>
2017-11-17 10:15:10 +0300

    Changed extends paging back to jparepository

--- src/main/java/com/mustafa/test/Test/IProductRepo.java
+++ src/main/java/com/mustafa/test/Test/IProductRepo.java
@@ -2,9 +2,9 @@ package com.mustafa.test.Test;
 
 import java.util.List;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IProductRepo extends PagingAndSortingRepository<Product, Long> {
+public interface IProductRepo extends JpaRepository<Product, Long> {
 
 	public List<Product> findAllByOrderByIdAsc();
 	
